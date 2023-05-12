b92a2319e2c52c1163c30af9822c11973c755aee
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-13 01:11:14 +0530

    Added solution - LeetHub

---
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    void bfs(vector<vector<int>> &v, vector<vector<bool>> &vis, vector<vector<int>> &dist, int x, int y)
+    {
+        queue<pair<int, int>> q;
+        q.push({x, y});
+        vis[x][y] = 1;
+        int dx[] = {0, 0, 1, -1};
+        int dy[] = {1, -1, 0, 0};
+
+        while (!q.empty())
+        {
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+
+            for (int p = 0; p < 4; p++)
+            {
+                for (int r = 0; r < 4; r++)
+                {
+                    int x1 = i + dx[p];
+                    int y1 = j + dy[r];
+                    if (x1 >= 0 and y1 >= 0 and x1 < v.size() and y1 < v[0].size() and !vis[x1][y1] and v[x1][y1] == 0)
+                    {
+                        q.push({x1, y1});
+                        if (v[x1][y1] == 1)
+                            dist[x1][y1] = 0;
+                        else
+                            dist[x1][y1] = dist[i][j] + 1;
+                        vis[x1][y1] = 1;
+                    }
+                }
+            }
+        }
+    }
+    vector<vector<int>> nearest(vector<vector<int>> &v)
+    {
+        int m = v.size(), n = v[0].size();
+        vector<vector<bool>> vis(m, vector<bool>(n, 0));
+        vector<vector<int>> dist(m, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (v[i][j] == 1 and !vis[i][j])
+                {
+                    dist[i][j] = 0;
+                    v[i][j] = 1;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        int dx[] = {0, 0, 1, -1};
+        int dy[] = {1, -1, 0, 0};
+
+        while (!q.empty())
+        {
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+
+            for (int k = 0; k < 4; k++)
+            {
+
+                int x1 = i + dx[k];
+                int y1 = j + dy[k];
+                if (x1 >= 0 and y1 >= 0 and x1 < v.size() and y1 < v[0].size() and !vis[x1][y1] and v[x1][y1] == 0)
+                {
+                    q.push({x1, y1});
+                    if (v[x1][y1] == 1)
+                        dist[x1][y1] = 0;
+                    else
+                        dist[x1][y1] = dist[i][j] + 1;
+                    vis[x1][y1] = 1;
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		vector<vector<int>> ans = obj.nearest(grid);
+		for(auto i: ans){
+			for(auto j: i){
+				cout << j << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
