7eaecfef6ce86a5ecd53eae7679948b6bf5a0495
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-29 01:37:13 +0530

    Create permutations-ii.cpp

---
+++ Permutations/permutations-ii.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/permutations-ii/
+
+class Solution {
+public:
+    void recur(set<vector<int>> &s,vector<int>& v,vector<int> curr,vector<bool> vis,int idx,int n)
+    {
+        if(idx==n)
+        {
+            s.insert(curr);
+            return;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            int num=v[i];
+            if(vis[i]) continue;
+            curr.push_back(num);
+            vis[i]=1;
+            recur(s,v,curr,vis,idx+1,n);
+            curr.pop_back();
+            vis[i]=0;
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& v) 
+    {
+        vector<vector<int>>res;
+        vector<int>curr;
+        vector<bool> vis(v.size(),0);
+        set<vector<int>>s;
+        recur(s,v,curr,vis,0,v.size());
+        for(auto it:s) res.push_back(it);
+        return res;    
+    }
+};
