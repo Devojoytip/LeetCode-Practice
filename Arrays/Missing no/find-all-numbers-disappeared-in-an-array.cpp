e59ba1b06fe5b830547aa371c61045e40a331eed
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-06-09 23:45:15 +0530

    Update find-all-numbers-disappeared-in-an-array.cpp

--- Arrays/Missing no/find-all-numbers-disappeared-in-an-array.cpp
+++ Arrays/Missing no/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,4 +1,12 @@
-// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
+/* 
+https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
+
+Since range [1-N] so we iterate over the arr
+Use idx = v[i]-1 as index and mark the value in v[idx] -ve
+If already -ve means we visited it earlier ie has duplicates as for each x in [1,N] we will reach v[x-1] once only if no duplicates
+after that we iterate again and if v[i] is +ve means we never reached it => we never got i+1 in the arr so push in res
+
+*/
 
 class Solution {
 public:
