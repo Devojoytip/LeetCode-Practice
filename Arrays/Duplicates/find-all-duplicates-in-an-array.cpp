47e138ba9f000429314eb1e992fc1f04fa1aea1a
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-06-09 23:43:49 +0530

    Update find-all-duplicates-in-an-array.cpp

--- Arrays/Duplicates/find-all-duplicates-in-an-array.cpp
+++ Arrays/Duplicates/find-all-duplicates-in-an-array.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/find-all-duplicates-in-an-array
 /*
+Same approach as finding missing elements
 
 Since range [1-N] so we iterate over the arr
 Use idx = v[i]-1 as index and mark the value in v[idx] -ve
