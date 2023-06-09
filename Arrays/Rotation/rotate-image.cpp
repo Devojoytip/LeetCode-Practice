d4f55b190d8919530a3b1662fd07d8f351df8c31
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-06-09 14:12:37 +0530

    Create rotate-image.cpp

---
+++ Arrays/Rotation/rotate-image.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/rotate-image/
+
+class Solution {
+public:
+    void transpose(vector<vector<int>>& v) {
+        int n=v.size();
+      
+        // O(N/2 * N/2)
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                swap(v[i][j],v[j][i]);
+            }
+        }
+        
+    }
+  
+    void rotate(vector<vector<int>>& v) {
+        
+        transpose(v);
+
+        // O(N * N/2)
+        for(auto &row: v) reverse(row.begin(),row.end());
+    }
+};
