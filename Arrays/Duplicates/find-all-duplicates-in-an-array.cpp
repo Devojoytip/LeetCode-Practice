ad8c57aa079c62c0d60a75ef1ddf06c435caf236
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-06-09 23:25:09 +0530

    Create find-all-duplicates-in-an-array.cpp

---
+++ Arrays/Duplicates/find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array
+/*
+
+Since range [1-N] so we iterate over the arr
+Use idx = v[i]-1 as index and mark the value in v[idx] -ve
+If already -ve means we visited it earlier ie has duplicates as for each x in [1,N] we will reach v[x-1] once only if no duplicates
+
+*/
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& v) {
+
+        vector<int> res;
+        
+        for(int i=0;i<v.size();i++)
+        {
+            int num=abs(v[i]);
+            if(v[num-1]>0) v[num-1]*=-1;
+            else res.push_back(num);
+        }
+        
+        return res;
+    }
+};
