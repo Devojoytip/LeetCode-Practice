9edf7d6c83242573ea805a0a522d9fab8594efb0
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-06-12 02:05:58 +0530

    Create check-if-a-parentheses-string-can-be-valid.cpp

---
+++ Stack/Parentheses/2 Stacks/check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid
+
+class Solution {
+public:
+    bool canBeValid(string s, string lock) {
+        int i=0;
+        stack<int> open;
+        stack<int> change;
+
+        while(i < s.size())
+        {
+            if(s[i]=='(' and lock[i]=='1') open.push(i);
+            
+            else if(s[i]=='(' and lock[i]=='0') change.push(i);
+            
+            else if(s[i]==')' and lock[i]=='0') change.push(i);
+
+            else
+            {
+                if(!open.empty()) open.pop();
+                else if(!change.empty()) change.pop();
+                else return 0;
+            }
+
+            i++;
+        }
+
+        while(!open.empty())
+        {
+            if(change.empty() or change.top()<open.top()) return 0;
+            else
+            {
+                open.pop();
+                change.pop();
+            }
+        }
+
+        if(change.size()%2==1) return false;
+
+        return 1;
+    }
+};
