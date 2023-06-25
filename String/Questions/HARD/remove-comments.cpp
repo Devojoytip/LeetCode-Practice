deb2233dd89399a06a272f1b566b5a3564f9a53f
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-06-26 00:02:58 +0530

    Create remove-comments.cpp

---
+++ String/Questions/HARD/remove-comments.cpp
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/remove-comments
+
+class Solution {
+public:
+    vector<string> removeComments(vector<string>& v) {
+        
+        vector<string> res;
+
+        string prev="",curr="";
+
+        for(auto s:v)
+        {
+            int i=0,n=s.size();
+            while(i<n)
+            {
+                if(prev=="/*")
+                {
+                    if(s[i]=='*' and i+1<n and s[i+1]=='/')
+                    {
+                        prev="";
+                        i+=2;   
+                    }
+                    else i++;
+                }
+                else if(s[i]=='/' and i+1<n and s[i+1]=='/')
+                {
+                    break;
+                }
+                else if(s[i]=='/' and i+1<n and s[i+1]=='*')
+                {
+                    prev="/*";
+                    i+=2;
+                }
+                else if(prev=="")
+                {
+                    curr+=s[i];
+                    i++;
+                }
+            }
+
+            if(prev=="")
+            {
+                if(curr!="") 
+                {
+                    res.push_back(curr);
+                    prev="";
+                    curr="";
+                }                
+            }
+        }
+
+        return res;
+    }
+};
