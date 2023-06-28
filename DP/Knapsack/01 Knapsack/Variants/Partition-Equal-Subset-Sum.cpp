1b4ea979cd97ac8ff373391a2d5c98dc7d41c00f
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-06-29 00:13:28 +0530

    Update Partition-Equal-Subset-Sum.cpp

--- DP/Knapsack/01 Knapsack/Variants/Partition-Equal-Subset-Sum.cpp
+++ DP/Knapsack/01 Knapsack/Variants/Partition-Equal-Subset-Sum.cpp
@@ -1,29 +1,29 @@
-// https://practice.geeksforgeeks.org/problems/subset-sum-problem2014/1
+// https://leetcode.com/problems/partition-equal-subset-sum
 
-class Solution{
+class Solution {
 public:
-    int equalPartition(int n, int arr[])
+    
+    bool sol(vector<int>& arr,int n,int sum,vector<vector<bool>> dp)
     {
-        // code here
-        int sum=0;
-        for(int i=0;i<n;i++) sum+=arr[i];
-        
-        if(sum%2==1) return 0;
-        
-        sum/=2;
-        
-        vector<vector<bool>> dp(n+1,vector<bool>(sum+1,0));
-        for(int i=0;i<=n;i++) dp[i][0]=1;
-        
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=sum;j++)
             {
-                if(arr[i-1]<=j) dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]];
-                else dp[i][j] = dp[i-1][j];
+                if(j>=arr[i-1]) dp[i][j]=dp[i-1][j-arr[i-1]] or dp[i-1][j];
+                else dp[i][j]=dp[i-1][j];
             }
         }
-        
         return dp[n][sum];
+    }
+
+    bool canPartition(vector<int>& arr) {
+        int n=arr.size();
+        int sum=0;
+        for(auto it:arr) sum+=it;
+        if(sum%2) return 0;
+        sum/=2;
+        vector<vector<bool>> dp(n+1,vector<bool>(sum+1,0));
+        dp[0][0]=1;
+        return sol(arr,n,sum,dp);
     }
 };
