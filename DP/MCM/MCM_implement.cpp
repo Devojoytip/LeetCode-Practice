2d652b575ce83a08e226df7c830e5091f8a24fe2
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-01 01:55:46 +0530

    Update MCM_implement.cpp

--- DP/MCM/MCM_implement.cpp
+++ DP/MCM/MCM_implement.cpp
@@ -3,12 +3,16 @@
 class Solution{
 public:
     int dp[101][101];
+
     int MCM(int a[], int i, int j)
     {
         // code here
-        int ans=INT_MAX;
         if(i>=j) return 0;
+        
         if(dp[i][j]!=-1) return dp[i][j];
+        
+        int ans=INT_MAX;
+        
         for(int k=i;k<=j-1;k++)
         {
             int temp=MCM(a,i,k) + MCM(a,k+1,j) + a[i-1]*a[k]*a[j];
@@ -22,6 +26,7 @@ public:
     {
         // code here
         memset(dp,-1,sizeof dp);
+        // start from 1 as A[0] = 1st matrix is a[0]*a[1]
         return MCM(a,1,n-1);
     }
 };
