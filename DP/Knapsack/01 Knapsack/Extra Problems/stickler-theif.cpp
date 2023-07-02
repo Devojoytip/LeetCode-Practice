dc65593dc5c6173e6d2ecc2e9bc16b9407d21f85
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-02 19:58:08 +0530

    Update stickler-theif.cpp

--- DP/Knapsack/01 Knapsack/Extra Problems/stickler-theif.cpp
+++ DP/Knapsack/01 Knapsack/Extra Problems/stickler-theif.cpp
@@ -29,3 +29,38 @@ class Solution
         
     }
 };
+
+// BOTTOM-UP
+
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int recur(int a[], int n)
+    {
+        // Your code here
+        int dp[n+1];
+        
+        memset(dp,-1,sizeof dp);
+        
+        for(int i=0;i<n;i++)
+        {
+            int take = (i>=2) ? dp[i-2] + a[i] : a[i];
+            
+            int notTake = (i>=1) ? dp[i-1] : 0;
+            
+            dp[i] = max(take, notTake);
+        }
+        
+        return dp[n-1];
+        
+    }
+    
+    int FindMaxSum(int a[], int n)
+    {
+        // Your code here
+        
+        return recur(a,n);
+        
+    }
+};
