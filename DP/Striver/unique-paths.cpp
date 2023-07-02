3242205a6dd1f84ecfc087f447e6f33505ba1c5a
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-02 23:32:05 +0530

    Update unique-paths.cpp

--- DP/Striver/unique-paths.cpp
+++ DP/Striver/unique-paths.cpp
@@ -21,3 +21,39 @@ public:
         return recur(0,0,m,n);
     }
 };
+
+// TABULATION / BOTTOM-UP
+
+class Solution {
+public:
+
+    int recur(int i,int j, int m, int n) 
+    {
+        int dp[101][101];
+        memset(dp,-1,sizeof dp);
+    
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 and j==0) dp[i][j] = 1;
+
+                else
+                {
+                    int left = (i==0) ? 0 : dp[i-1][j];
+
+                    int up = (j==0) ? 0 : dp[i][j-1];
+
+                    dp[i][j] = left + up;
+                }
+            }
+        }
+    
+        return dp[m-1][n-1];
+    }
+
+    int uniquePaths(int m, int n) {
+        
+        return recur(0,0,m,n);
+    }
+};
