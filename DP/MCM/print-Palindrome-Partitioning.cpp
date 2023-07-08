57df1e79bc95d2098da7041d26a745c9eb64046b
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-08 22:18:35 +0530

    Update print-Palindrome-Partitioning.cpp

--- DP/MCM/print-Palindrome-Partitioning.cpp
+++ DP/MCM/print-Palindrome-Partitioning.cpp
@@ -1,35 +1,44 @@
 // https://leetcode.com/problems/palindrome-partitioning/description/
 
+// Simple Recursion
+
 class Solution {
 public:
-    vector<vector<string>> res;
-    bool isPalindrome(string s)
+
+    bool isPal(string s)
     {
-        string a=s;
+        string a = s;
         reverse(s.begin(),s.end());
         return a==s;
     }
-    void sol(string s,int i,vector<string> v)
-    {
-        if(i==s.length())
-        {
-            res.push_back(v);
-            return;
-        }
-        for(int k=i;k<s.length();k++)
-        {
-            string s1=s.substr(i,k-i+1);
-            if(isPalindrome(s1)) 
-            {
-                v.push_back(s1);
-                sol(s,k+1,v);
-                v.pop_back();
-            }
-        }
+
+    void recur(string s, vector<string> v, vector<vector<string>> &res, int i, int n) {
+
+       if(i==n) 
+       {
+           res.push_back(v);
+           return;
+       }
+
+       for(int k=i;k<n;k++)
+       {
+           string curr=s.substr(i,k-i+1);
+
+           if(isPal(curr))
+           {
+               v.push_back(curr);
+               recur(s,v,res,k+1,n);
+               v.pop_back();
+           }
+       }
     }
+
     vector<vector<string>> partition(string s) {
-        vector<string> v;
-        sol(s,0,v);
+        
+        vector<vector<string>> res;
+
+        recur(s,{},res,0,s.size());
+
         return res;
     }
 };
