ebd40787929e03036b0290b935d066c45b99c6c7
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-18 10:13:54 +0530

    Update different-ways-to-add-parentheses.cpp

--- Recursion/Expression Trees/different-ways-to-add-parentheses.cpp
+++ Recursion/Expression Trees/different-ways-to-add-parentheses.cpp
@@ -1,52 +1,50 @@
 // https://leetcode.com/problems/different-ways-to-add-parentheses
 
+// Expression tree - using for loop to find all left and right possible results for the curr operand ie (........) + (.........)
+
 class Solution {
 public:
-    int eval(int a,int b,char c)
+    int eval(int a, int b, char c)
     {
         switch(c)
         {
             case '+':
                 return a+b;
-            
             case '-':
                 return a-b;
-            
             case '*':
                 return a*b;
-
             default:
-                return 0;
+                return -1;
         }
     }
 
-    vector<int> sol(string s) 
-    {
+    vector<int> recur(string s) {
+        
+        vector<int> res;
 
-       vector<int> v;
-       int i=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(isdigit(s[i])) continue;
 
-       for(int i=0; i<s.size(); i++)
-       {
-           if(isdigit(s[i])) continue;
+            vector<int> left=recur(s.substr(0,i));
+            vector<int> right=recur(s.substr(i+1));
 
-           vector<int> left=sol(s.substr(0,i));
-           vector<int> right=sol(s.substr(i+1));
+            for(auto a: left)
+            {
+                for(auto b: right)
+                {
+                    res.push_back(eval(a,b,s[i]));
+                }
+            }
+        }
 
-           for(auto a: left)
-           {
-               for(auto b: right)
-               {
-                   v.push_back(eval(a,b,s[i]));
-               }
-           }
-       }
+        if(res.empty()) res.push_back(stoi(s)); // single num
 
-       if(v.empty()) v.push_back(stoi(s));
-
-       return v;
+        return res;
     }
+
     vector<int> diffWaysToCompute(string s) {
-        return sol(s);
+        return recur(s);    
     }
 };
