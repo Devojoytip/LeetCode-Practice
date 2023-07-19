4045cac73b43edfb69d1988716a3c36a658b814d
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-19 22:37:49 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,23 +9,26 @@ using namespace std;
 
 class Solution{   
 public:
+    bool f(vector<int>v, int i, int n, int s, vector<vector<int>> &dp){
+        // code here 
+        if(s==0) return 1;
+        
+        if(i==n) return 0;
+        
+        if(dp[i][s]!=-1) return dp[i][s];
+        
+        if(v[i]<=s) dp[i][s]= f(v,i+1,n,s,dp) or f(v,i+1,n,s-v[i],dp);
+        
+        else dp[i][s]= f(v,i+1,n,s,dp);
+        
+        return dp[i][s];
+    }
+    
     bool isSubsetSum(vector<int>v, int sum){
         // code here 
         int n=v.size();
-        
-        vector<vector<bool>> dp(n+1,vector<bool> (sum+1,0));
-        for(int i=0;i<=n;i++) dp[i][0]=1;
-        
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=sum;j++)
-            {
-                if(v[i-1]<=j) dp[i][j]= dp[i-1][j] or dp[i-1][j-v[i-1]];
-                else dp[i][j]= dp[i-1][j];
-            }
-        }
-        
-        return dp[n][sum];
+        vector<vector<int>> dp(n+1, vector<int>(sum+1,-1));
+        return f(v,0,n,sum,dp);
     }
 };
 
