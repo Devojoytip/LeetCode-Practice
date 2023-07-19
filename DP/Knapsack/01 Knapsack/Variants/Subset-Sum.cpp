5b7804bd616753306f997e65d9cf354fa3258aca
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-20 00:56:00 +0530

    Update Subset-Sum.cpp

--- DP/Knapsack/01 Knapsack/Variants/Subset-Sum.cpp
+++ DP/Knapsack/01 Knapsack/Variants/Subset-Sum.cpp
@@ -3,6 +3,31 @@
 
 class Solution{   
 public:
+    bool f(vector<int>v, int i, int n, int s, vector<vector<int>> &dp){
+        // code here 
+        if(s==0) return 1;
+        
+        if(i==n) return 0;
+        
+        if(dp[i][s]!=-1) return dp[i][s];
+        
+        if(v[i]<=s) dp[i][s]= f(v,i+1,n,s,dp) or f(v,i+1,n,s-v[i],dp);
+        
+        else dp[i][s]= f(v,i+1,n,s,dp);
+        
+        return dp[i][s];
+    }
+    
+    bool isSubsetSum(vector<int>v, int sum){
+        // code here 
+        int n=v.size();
+        vector<vector<int>> dp(n+1, vector<int>(sum+1,-1));
+        return f(v,0,n,sum,dp);
+    }
+};
+
+class Solution{   
+public:
     bool isSubsetSum(vector<int>v, int sum){
         // code here 
         int n=v.size();
