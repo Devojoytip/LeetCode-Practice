d25410883e62c653b4452252f58306997baa620d
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-20 01:32:29 +0530

    Update coin-change-ii.cpp

--- DP/Knapsack/Unbounded Knapsack/coin-change-ii.cpp
+++ DP/Knapsack/Unbounded Knapsack/coin-change-ii.cpp
@@ -2,6 +2,33 @@
 
 // Count no of subsets with given sum 
 
+// TD
+class Solution {
+public:
+int f(int i, int n, int w, vector<int>& v, vector<vector<int>> &dp)
+    {
+        // code here
+        if(w==0) return 1;
+
+        if(i>=n) return 0;
+        
+        if(dp[i][w]!=-1) return dp[i][w];
+        
+        if(w>=v[i]) dp[i][w] = f(i,n,w-v[i],v,dp) + f(i+1,n,w,v,dp);
+        
+        else dp[i][w] = f(i+1,n,w,v,dp);
+        
+        return dp[i][w];
+    }
+    
+    int change(int t, vector<int>& v) {
+        int n=v.size();
+        vector<vector<int>> dp(n+1,vector<int>(t+1,-1));
+
+        return f(0,n,t,v,dp);
+    }
+};
+
 class Solution {
 public:
 int change(int sum, vector<int>& v) {
