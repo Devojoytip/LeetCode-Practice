103f49e62dd072b205951f1588cc2ff49bb2f503
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-19 22:50:14 +0530

    Update target-sum.cpp

--- DP/Knapsack/01 Knapsack/Variants/target-sum.cpp
+++ DP/Knapsack/01 Knapsack/Variants/target-sum.cpp
@@ -1,5 +1,42 @@
 // https://leetcode.com/problems/target-sum
 
+// BOTTOM-UP
+class Solution {
+public:
+    int recur(vector<int>& v, int i, int n, int t, vector<vector<int>> &dp) {
+
+        if(t==0 and i==n) return 1;
+
+        if(i>=n) return 0;
+
+        if(dp[i][t]!=-1) return dp[i][t];
+
+        if(v[i]<=t) dp[i][t]=recur(v,i+1,n,t-v[i],dp)+recur(v,i+1,n,t,dp);
+        
+        else dp[i][t]=recur(v,i+1,n,t,dp);
+
+        return dp[i][t];
+    }
+
+    int findTargetSumWays(vector<int>& v, int target) {
+
+        int n=v.size(), sum=0;
+
+        for(auto it: v) sum+=it;
+
+        int t=sum+target;
+
+        if(t%2 or t<0) return 0;
+
+        t/=2;
+
+        vector<vector<int>> dp(n+1, vector<int>(t+1,-1));
+
+        return recur(v,0,n,t,dp);
+    }
+};
+
+// TOP-DOWN
 class Solution {
     int solve(vector<int>& arr, int n, int sum,vector<vector<int>> &dp){
         for(int i=0;i<=n;i++) dp[i][0]=1;
