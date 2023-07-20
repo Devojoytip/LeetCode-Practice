ab21ba77e107b5f20ec53b819fffa4810c001d2a
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-20 10:01:23 +0530

    Update implement.cpp

--- DP/Knapsack/Unbounded Knapsack/implement.cpp
+++ DP/Knapsack/Unbounded Knapsack/implement.cpp
@@ -1,6 +1,31 @@
 // https://practice.geeksforgeeks.org/problems/knapsack-with-duplicate-items4201/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
 
 class Solution{
+  public:
+    int f(int price[], int i, int n, vector<vector<int>> &dp) {
+        //code here
+        
+        if(i<0 or n==0) return 0;
+        
+        if(dp[i][n]!=-1) return dp[i][n];
+        
+        if(n>=i+1) dp[i][n] = max(f(price,i,n-i-1,dp)+price[i], f(price,i-1,n,dp));
+        
+        else dp[i][n] = f(price,i-1,n,dp);
+        
+        return dp[i][n];
+    }
+    
+    int cutRod(int price[], int n) {
+        //code here
+        
+        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
+        
+        return f(price,n-1,n,dp);
+    }
+};
+
+class Solution{
 public:
     int f(int i, int n, int w, int val[], int wt[], vector<vector<int>> &dp)
     {
