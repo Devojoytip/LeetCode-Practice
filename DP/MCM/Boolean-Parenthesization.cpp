f7a5ce1d1d6108f161779ba1fe568bb79909ed2f
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-21 09:31:23 +0530

    Update Boolean-Parenthesization.cpp

--- DP/MCM/Boolean-Parenthesization.cpp
+++ DP/MCM/Boolean-Parenthesization.cpp
@@ -4,50 +4,59 @@
 
 class Solution{
 public:
-    int dp[305][305][5];
-    int sol(string s,int i,int j,bool inp)
-    {
-        if(i>j)return 0;
-        int z=0;
-        if(inp) z=1;
+    int f(int n, string s, int z, int i, int j, int dp[][201][2]){
+        // code here
+        
+        if(i>j) return 0;
         
         if(i==j)
         {
-            if(inp and s[i]=='T') return dp[i][j][z]=1; 
-            if(!inp and s[i]=='F') return dp[i][j][z]=1;
+            if(z and s[i]=='T') return dp[i][j][z]=1; 
+            if(!z and s[i]=='F') return dp[i][j][z]=1;
             return 0; 
         }
         
         if(dp[i][j][z]!=-1) return dp[i][j][z];
+        
         int ans=0;
+        
         for(int k=i+1;k<j;k+=2)
         {
-            int lt=sol(s,i,k-1,1);
-            int lf=sol(s,i,k-1,0);
-            int rt=sol(s,k+1,j,1);
-            int rf=sol(s,k+1,j,0);
-            if(s[k]=='|') 
+            int lt=f(n,s,1,i,k-1,dp);
+            int lf=f(n,s,0,i,k-1,dp);
+            int rt=f(n,s,1,k+1,j,dp);
+            int rf=f(n,s,0,k+1,j,dp);
+            
+            char c=s[k];
+            
+            if(c=='^')
             {
-                if(inp) ans+=lf*rt+lt*rt+lt*rf;
-                else ans+=lf*rf;
+                if(z) ans+=lt*rf + lf*rt;
+                else ans+=lt*rt + lf*rf;
             }
-            else if(s[k]=='&') 
+            
+            else if(c=='|')
             {
-                if(inp) ans+=lt*rt;
-                else ans+=lf*rt+lf*rf+lt*rf;
+                if(z) ans+=lt*rf + lf*rt + lt*rt;
+                else ans+= lf*rf;
             }
+            
             else
             {
-                if(inp) ans+=lt*rf+lf*rt;
-                else ans+=lt*rt+lf*rf;
+                if(z) ans+=lt*rt;
+                else ans+=lt*rf + lf*rt + lf*rf;
             }
         }
         
         return dp[i][j][z]=ans%1003;
     }
-    int countWays(int n, string s){
+    
+    int countWays(int N, string S){
         // code here
+        
+        int dp[201][201][2];
         memset(dp,-1,sizeof dp);
-        return sol(s,0,s.length()-1,1);
+        
+        return f(N,S,1,0,N-1,dp);
     }
 };
