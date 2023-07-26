510de689039bb2b1abff47617bfb50d68bc91c5b
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-27 00:43:52 +0530

    Update combination-sum.cpp

--- Recursion/Combination Sum/combination-sum.cpp
+++ Recursion/Combination Sum/combination-sum.cpp
@@ -1,31 +1,27 @@
-// 
+// https://leetcode.com/problems/combination-sum
 
 class Solution {
 public:
-    void recur(vector<int>& v,vector<vector<int>> &res,vector<int> curr, int idx, int sum) 
-    {
-        if(sum==0)
+    void f(vector<int>& v, vector<int> curr, vector<vector<int>> &res, int i, int t) {
+        
+        if(t==0)
         {
             res.push_back(curr);
             return;
         }
-        
-        if(idx>=v.size() or sum<0) return;
 
-        for(int i=idx;i<v.size();i++)
-        {
-            curr.push_back(v[i]);
-            recur(v,res,curr,i,sum-v[i]);
-            curr.pop_back();
-        }
+        if(i==v.size() or t<0) return;
+
+        f(v,curr,res,i+1,t);
+
+        curr.push_back(v[i]);
+        f(v,curr,res,i,t-v[i]);
     }
 
-    vector<vector<int>> combinationSum(vector<int>& v, int sum) {
+    vector<vector<int>> combinationSum(vector<int>& v, int t) {
         
         vector<vector<int>> res;
-        vector<int> curr;
-
-        recur(v,res,curr,0,sum);
+        f(v,{},res,0,t);
 
         return res;
     }
