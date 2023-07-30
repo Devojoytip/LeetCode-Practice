3db871ae7ee56ff7b0657d54164c1f2a9527673e
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-07-30 08:59:42 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -3,45 +3,45 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	
-	// BFS - Kahn's Algo
-	
-	vector<int> topoSort(int n, vector<int> v[]) 
+	void dfs(int n, int curr, vector<int> &vis, stack<int> &s, vector<int> adj[]) 
 	{
 	    // code here
+	    vis[curr]=1;
+	    
+	    for(auto neigh: adj[curr]) 
+	    {
+	        if(!vis[neigh]) dfs(n,neigh,vis,s,adj);
+	    }
+	    s.push(curr);
+	}
+	
+	vector<int> topoSort(int n, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int> vis(n,0);
 	    vector<int> res;
-	    vector<int> in_degree(n,0);
 	    stack<int> s;
 	    
 	    for(int i=0;i<n;i++)
 	    {
-	        for(auto neigh:v[i]) in_degree[neigh]++;
+	        if(!vis[i]) dfs(n,i,vis,s,adj);
 	    }
 	    
-	    for(int i=0;i<n;i++)
+	    while(s.size())
 	    {
-	        if(in_degree[i]==0) s.push(i);
-	    }
-	    
-	    while(!s.empty())
-	    {
-	        int i=s.top();
-	        res.push_back(i);
+	        res.push_back(s.top());
 	        s.pop();
-	        for(auto neigh:v[i]) 
-	        {
-	            if(in_degree[neigh]!=0) in_degree[neigh]--;
-	            if(in_degree[neigh]==0) s.push(neigh);
-	        }
 	    }
 	    
 	    return res;
 	}
 };
+
 
 //{ Driver Code Starts.
 
