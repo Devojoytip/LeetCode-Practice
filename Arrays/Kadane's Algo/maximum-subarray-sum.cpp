f9051e4e19b7987299adccb15d47a28d10504cdc
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-08-01 00:21:33 +0530

    Update maximum-subarray-sum.cpp

--- Arrays/Kadane's Algo/maximum-subarray-sum.cpp
+++ Arrays/Kadane's Algo/maximum-subarray-sum.cpp
@@ -1,19 +1,25 @@
 // https://leetcode.com/problems/maximum-subarray
 // Kadane's Algo
 
-class Solution {
-public:
-    int maxSubArray(vector<int>& v) {
-
-      int n=v.size(),maxi=INT_MIN,sum=0;
-
-      for(int i=0;i<n;i++)
-      {
-        sum+=v[i];
-        maxi=max(sum,maxi);
-        if(sum<0) sum=0;
-      }
-
-      return maxi;
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int a[], int n){
+        
+        // Your code here
+        long long sum=0, ans=-1e9;
+        
+        for(int i=0;i<n;i++) 
+        {
+            sum+=a[i];
+            
+            ans=max(ans, sum);
+            
+            if(sum<0) sum=0;
+        }
+        
+        return ans;
     }
 };
