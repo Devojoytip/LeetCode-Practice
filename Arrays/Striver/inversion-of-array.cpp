00911da8a3709ccf87a8e779c97aff0f37ef25da
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-08-01 00:06:37 +0530

    Create inversion-of-array.cpp

---
+++ Arrays/Striver/inversion-of-array.cpp
@@ -0,0 +1,80 @@
+// https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1
+
+#define ll long long
+class Solution{
+  public:
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    // Function to count inversions in the array.
+    ll merge(ll a[], ll s, ll mid, ll e, ll n)
+    {
+        ll i=s, j=mid+1, cnt=0;
+        
+        while(i<=mid)
+        {
+            cnt+=j-(mid+1);
+            while(j<=e and a[i]>a[j]) cnt++,j++;
+            i++;
+        }
+        
+        vector<ll> v;
+        i=s; 
+        j=mid+1;
+        
+        while(i<=mid and j<=e)
+        {
+            if(a[i]<a[j])
+            {
+                v.push_back(a[i]);
+                i++;
+            }
+            else
+            {
+                v.push_back(a[j]);
+                j++;
+            }
+        }
+        
+        while(i<=mid)
+        {
+            v.push_back(a[i]);
+            i++;
+        }
+        
+        while(j<=e)
+        {
+            v.push_back(a[j]);
+            j++;
+        }
+        
+        for(ll k=s;k<=e;k++)
+        {
+            a[k]=v[k-s];
+        }
+        
+        return cnt;
+    }
+    
+    ll mergeSort(ll a[], ll s, ll e, ll n)
+    {
+        // Your Code Here
+        if (s >= e) return 0;
+        
+        ll mid=(s+e)/2;
+        
+        ll ans = mergeSort(a,s,mid,n);
+        
+        ans += mergeSort(a,mid+1,e,n);
+        
+        ans += merge(a,s,mid,e,n);
+        
+        return ans;
+    }
+     
+    ll int inversionCount(ll a[], ll n)
+    {
+        // Your Code Here
+        return mergeSort(a,0,n-1,n);
+    }
+
+};
