5bc48ed39912df49c27817bc3cd31ba6113eb76f
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-08-10 10:02:05 +0530

    Added solution - LeetHub

---
+++ Floor in BST - GFG/floor-in-bst.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+int floor(Node *root, int x);
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+
+        cout << floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
+
+
+// Function to search a node in BST.
+
+int floor(Node* r, int x) {
+    // Code here
+    
+    if(!r) return -1;
+    
+    if(r->data<=x and !r->left and !r->right) return r->data; 
+    
+    if(r->data>x and !r->left and !r->right) return -1;
+    
+    if(r->data<=x) return max(r->data, floor(r->right, x));
+    
+    return floor(r->left, x);
+}
