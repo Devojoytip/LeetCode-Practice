14ed0ed5c17b739e5726aaa2617b05ea96a8a01e
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-08-24 23:38:47 +0530

    Update lru-cache.cpp

--- LRU Cache/lru-cache.cpp
+++ LRU Cache/lru-cache.cpp
@@ -4,71 +4,72 @@
 Doubly linked list and map
 
 */
+
 class LRUCache {
 public:
     class Node
     {
         public:
-        int val;
-        int key;
-        Node*next;
-        Node*prev;
-        Node(int key,int val)
+        Node *prev, *next;
+        int key, val;
+
+        Node(int key, int val)
         {
             this->key=key;
             this->val=val;
+            this->prev=NULL;
+            this->next=NULL;
         }
     };
-    
-    int n;
-    map<int,Node*> mp;
-    Node*head=new Node(0,0);
-    Node*tail=new Node(0,0);
 
+    Node* head=new Node(-1,-1);
+    Node* tail=new Node(-1,-1);
+    int capacity;
+    map<int, Node*> mp;
+    
     LRUCache(int capacity) {
-        n=capacity;
-        head->next = tail;
-        tail->prev = head;
-    }
-
-    void addNode(Node* root)
-    {
-        mp[root->key]=root;
-        Node* temp=head->next;
-        head->next=root;
-        root->next=temp;
-        temp->prev=root;
-        root->prev=head;
-    }
-
-    void deleteNode(Node* root)
-    {
-        mp.erase(root->key);
-        Node* r1 = root->prev;
-        Node* r2 = root->next;
-        r1->next = r2;
-        r2->prev = r1; 
+        this->capacity=capacity;
+        head->next=tail;
+        tail->prev=head;
     }
     
+    void addNode(Node* temp)
+    {
+        Node* nextNode=head->next;
+        head->next=temp;
+        temp->next=nextNode;
+        temp->prev=head;
+        nextNode->prev=temp;
+        mp[temp->key]=temp;
+    }
+
+    void deleteNode(Node* temp)
+    {
+        Node* prevNode=temp->prev;
+        Node* nextNode=temp->next;
+        prevNode->next=nextNode;
+        nextNode->prev=prevNode;
+        mp.erase(temp->key);
+        delete temp;
+    }
+
     int get(int key) {
-        if(mp.count(key)==0) return -1;
-        Node* root = mp[key];
-        deleteNode(root);
-        addNode(root);
-        return root->val;
+        
+        if(mp.find(key)==mp.end()) return -1;
+        
+        Node* temp=mp[key];
+        int val=temp->val;
+
+        deleteNode(temp);
+        addNode(new Node(key, val));
+        
+        return val;
     }
     
     void put(int key, int value) {
-        Node* root = new Node(key,value);
-        if(mp.count(key)!=0) deleteNode(mp[key]); // update the LRU ie the node becomes most recent so delete it and again add new
-        if(mp.size()==n) deleteNode(tail->prev);  // cache is full so del least used ie tail's prev
-        addNode(root); 
+        
+        if(mp.find(key)!=mp.end()) deleteNode(mp[key]);
+        if(mp.size()==capacity) deleteNode(tail->prev);
+        addNode(new Node(key, value));
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
