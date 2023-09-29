b5194250f17473294b447071622e6b4986634ba2
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-09-29 15:20:22 +0530

    Update longest-arithmetic-subsequence.cpp

--- DP/DP (Max - Min)/longest-arithmetic-subsequence.cpp
+++ DP/DP (Max - Min)/longest-arithmetic-subsequence.cpp
@@ -6,7 +6,8 @@ public:
 
         int n=v.size(),ans=0;
         vector<vector<int>> dp(n+1,vector<int>(1001,1));
-        
+
+        // 1st type loop
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<i;j++)
@@ -14,6 +15,19 @@ public:
                 int diff=v[i]-v[j];
                 dp[i][diff+500]=max(dp[i][diff+500], 1+dp[j][diff+500]);
                 ans=max(ans,dp[i][500+diff]);
+            }
+        }
+
+        // 2nd type loop
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int diff=v[j]-v[i];
+                int idx=diff+500;
+
+                if(dp[j][idx]<1+dp[i][idx]) dp[j][idx]=1+dp[i][idx];
+                ans=max(ans, dp[j][idx]);
             }
         }
 
