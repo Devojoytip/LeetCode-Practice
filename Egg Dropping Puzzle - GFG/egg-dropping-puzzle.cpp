8d3c42238ffcc52c0010efcb0f3b9a53b55e1c8f
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-10-20 11:04:24 +0530

    Added solution - LeetHub

--- Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
+++ Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
@@ -8,31 +8,27 @@ class Solution
     public:
     //Function to find minimum number of attempts needed in 
     //order to find the critical floor.
-    
-    int sol(int n, int f, vector<vector<int>> &dp) 
-    {
-        if(f==0 or f==1) return f;
-        
-        if(n==1) return f;
-        
-        if(dp[n][f]!=-1) return dp[n][f];
-        
-        int ans=1e9;
-        for(int k=1;k<=f;k++)
-        {
-            dp[n][f]=1+max(sol(n-1,k-1,dp), sol(n,f-k,dp));
-            ans=min(ans, dp[n][f]);
-        }
-        
-        return dp[n][f]=ans;
-    }
-    
-    int eggDrop(int n, int f) 
+    int eggDrop(int e, int f) 
     {
         // your code here
-        vector<vector<int>> dp(n+1,vector<int>(f+1, -1));
+        vector<vector<int>> dp(e+1, vector<int>(f+1, -1));
+        return func(e,f,dp);
+    }
+    
+    int func(int e, int f, vector<vector<int>> &dp) 
+    {
+        // your code here
+        if(e==1) return f;
         
-        return sol(n,f,dp);
+        if(f<=1) return f;
+        
+        if(dp[e][f]!=-1) return dp[e][f];
+        
+        int ans=1e9;
+        
+        for(int k=1;k<=f;k++) ans=min(ans, 1+max(func(e-1,k-1,dp), func(e,f-k,dp)));
+        
+        return dp[e][f]=ans;
     }
 };
 
