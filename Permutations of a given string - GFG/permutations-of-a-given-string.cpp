6e34846630c78e07c57cfdf24477c8143154e9d2
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-11-04 23:08:04 +0530

    Added solution - LeetHub

--- Permutations of a given string - GFG/permutations-of-a-given-string.cpp
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -7,42 +7,45 @@ using namespace std;
 class Solution
 {
 	public:
-		void f(string s, string curr, int cnt, vector<string> &res, vector<int> &vis)
-		{
-		    // Code here there
-		    if(cnt==s.size())
-		    {
-		        res.push_back(curr);
-		        return;
-		    }
-		    
-		    unordered_map<char, int> mp; // ensure same char doesn't come again
-		    
-		    for(int i=0;i<s.size();i++)
-		    {
-		        if(mp.find(s[i])!=mp.end() or vis[i]) continue;
-		        mp[s[i]]=1;
-		        vis[i]=1;
-	            cnt++;
-	            f(s,curr+s[i],cnt,res,vis);
-	            vis[i]=0;
-	            cnt--;
-		    }
-		}
-		
+	    void f(string s, string t, vector<string> &res, int cnt, vector<int> vis)
+	    {
+	        if(cnt==s.size())
+	        {
+	            res.push_back(t);
+	            return;
+	        }
+	        
+	       // for unique permutation ensure same char does not come at the same index/position 
+	       
+	       unordered_map<char,int> curr_vis;
+	       
+	       for(int i=0;i<s.size();i++)
+	       {
+	           if(curr_vis[s[i]] or vis[i]) continue;
+	           
+	           curr_vis[s[i]]=1;
+	           vis[i]=1;
+	           f(s,t+s[i],res,cnt+1,vis);
+	           vis[i]=0;
+	       }
+	    }
+	    
 		vector<string>find_permutation(string s)
 		{
 		    // Code here there
 		    vector<string> res;
-		    vector<int> vis(26, 0);
-		    f(s,"",0,res,vis);
-		    sort(res.begin(), res.end(), [](const string &a, const string &b)
+		    vector<int> vis(26,0);
+		    f(s,"",res,0,vis);
+		    
+		    sort(res.begin(),res.end(),[](const string &a, const string &b)
 		    {
 		        return a<b;
 		    });
+		    
 		    return res;
 		}
 };
+
 
 
 //{ Driver Code Starts.
